8991213447a11188fedbd431bb92845264552826
zhukai <zhukai@homolo.com>
2023-05-06 21:49:49 +0800

    feat: 添加docker启动方式

--- src/main.ts
+++ src/main.ts
@@ -53,36 +53,41 @@ async function main() {
 main();
 
 app.post("/notify", async (req: Request, res: Response): Promise<Response> => {
-  const state = req.body.state;
-  const i = state.indexOf(":");
-  const roomName = state.substring(0, i);
-  const userName = state.substring(i + 1);
-  const room = await client.Room.find({ topic: roomName });
-  if (!room) {
-    return res.status(404).send("room not found");
-  }
-  const action = req.body.action;
-  const status = req.body.status;
-  const description = req.body.description;
-  if (status == 'IN_PROGRESS') {
-    room.say(`@${userName} \n✅ 您的任务已提交\n✨ ${description}\n🚀 正在快速处理中，请稍后`);
-  } else if (status == 'FAILURE') {
-    room.say(`@${userName} \n❌ 任务执行失败\n✨ ${description}`);
-  } else if (status == 'SUCCESS') {
-    const time = req.body.finishTime - req.body.submitTime;
-    if (action == 'UPSCALE') {
-      await room.say(`@${userName} \n🎨 图片放大，用时: ${displayMilliseconds(time)}\n✨ ${description}`);
-      const image = FileBox.fromUrl(req.body.imageUrl);
-      room.say(image);
-    } else {
-      const taskId = req.body.id;
-      const prompt = req.body.prompt;
-      await room.say(`@${userName} \n🎨 ${action == 'IMAGINE' ? '绘图' : '变换'}成功，用时 ${displayMilliseconds(time)}\n✨ Prompt: ${prompt}\n📨 任务ID: ${taskId}\n🪄 放大 U1～U4 ，变换 V1～V4\n✏️ 使用[/up 任务ID 操作]\n/up ${taskId} U1`);
-      const image = FileBox.fromUrl(req.body.imageUrl);
-      room.say(image);
+  try {
+    const state = req.body.state;
+    const i = state.indexOf(":");
+    const roomName = state.substring(0, i);
+    const userName = state.substring(i + 1);
+    const room = await client.Room.find({ topic: roomName });
+    if (!room) {
+      return res.status(404).send("room not found");
     }
+    const action = req.body.action;
+    const status = req.body.status;
+    const description = req.body.description;
+    if (status == 'IN_PROGRESS') {
+      room.say(`@${userName} \n✅ 您的任务已提交\n✨ ${description}\n🚀 正在快速处理中，请稍后`);
+    } else if (status == 'FAILURE') {
+      room.say(`@${userName} \n❌ 任务执行失败\n✨ ${description}`);
+    } else if (status == 'SUCCESS') {
+      const time = req.body.finishTime - req.body.submitTime;
+      if (action == 'UPSCALE') {
+        await room.say(`@${userName} \n🎨 图片放大，用时: ${displayMilliseconds(time)}\n✨ ${description}`);
+        const image = FileBox.fromUrl(req.body.imageUrl);
+        room.say(image);
+      } else {
+        const taskId = req.body.id;
+        const prompt = req.body.prompt;
+        await room.say(`@${userName} \n🎨 ${action == 'IMAGINE' ? '绘图' : '变换'}成功，用时 ${displayMilliseconds(time)}\n✨ Prompt: ${prompt}\n📨 任务ID: ${taskId}\n🪄 放大 U1～U4 ，变换 V1～V4\n✏️ 使用[/up 任务ID 操作]\n/up ${taskId} U1`);
+        const image = FileBox.fromUrl(req.body.imageUrl);
+        room.say(image);
+      }
+    }
+    return res.status(200).send({ code: 1 });
+  } catch (e) {
+    console.error(`notify callback failed: ${e}`);
+    return res.status(500).send({ code: -9 });
   }
-  return res.status(200).send({ code: 1 });
 });
 
 try {
